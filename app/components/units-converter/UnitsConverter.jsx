810afb53ed2dcb8a852fff0c6ac5e6c555ea5deb
alexkham <alexkha@netvision.net.il>
2024-01-19 10:45:06 +0200

    reamore

--- app/components/units-converter/UnitsConverter.jsx
+++ app/components/units-converter/UnitsConverter.jsx
@@ -95,8 +95,8 @@ function UnitsConverter({conversionType}) {
   'Celsius': 'degC',
   'Fahrenheit': 'degF',
   'Kelvin': 'degK',
-  'Rankine': 'degR',
-  'Réaumur': 'degRe',
+  'Rankine': 'degR'
+  // 'Réaumur': 'degRe',
 
  }
 
